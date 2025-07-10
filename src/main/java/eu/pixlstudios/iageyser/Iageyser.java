11d73adf3a652c34538cf113ea77136b0de64e1e
pro6836 <aurtyytt@gmail.com>
2025-07-10 12:37:58 -0400

    1s Edit! EPIC

---
+++ src/main/java/eu/pixlstudios/iageyser/Iageyser.java
@@ -0,0 +1,17 @@
+package eu.pixlstudios.iageyser;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Iageyser extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
