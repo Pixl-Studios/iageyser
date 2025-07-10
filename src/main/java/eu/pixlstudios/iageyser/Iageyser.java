a569aadf99fb58fac808a4e093594993ff0ba173
pro6836 <aurtyytt@gmail.com>
2025-07-10 16:35:57 -0400

    Added config file logic, added TODOs for other logic

--- src/main/java/eu/pixlstudios/iageyser/Iageyser.java
+++ src/main/java/eu/pixlstudios/iageyser/Iageyser.java
@@ -1,48 +1,105 @@
 package eu.pixlstudios.iageyser;
 
 import com.google.common.eventbus.Subscribe;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import dev.lone.itemsadder.api.CustomBlock;
 import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.Events.ResourcePackSendEvent;
 import dev.lone.itemsadder.api.ItemsAdder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.geysermc.geyser.api.GeyserApi;
 import org.geysermc.geyser.api.block.custom.CustomBlockData;
 import org.geysermc.geyser.api.event.lifecycle.GeyserDefineCustomItemsEvent;
 import org.geysermc.geyser.api.event.lifecycle.GeyserPostInitializeEvent;
 import org.geysermc.geyser.api.item.custom.CustomItemOptions;
+import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public final class Iageyser extends JavaPlugin {
+public final class Iageyser extends JavaPlugin implements Listener {
 
     private Object CustomItemOptions;
-
-    @Override
-    public void onEnable() {
-        getLogger().info("Iageyser enabled!");
+    FileConfiguration config = getConfig();
+    private boolean GeyserLoaded = false;
+    public void sendPackToServer(String URL) {
+        //TODO: SEND URL TO SERVER FOR PROCCESSING
+    }
+    public void reloadPlugin() {
+        //TODO: Implement Reload Plugin Logic
     }
     @Override
+    public void onEnable() {
+        getLogger().info("IaGeyser enabled!");
+        config.addDefault("serverurl", "CHANGEME");
+        config.addDefault("enabled", true);
+        config.addDefault("restart_on_convert", false);
+        config.addDefault("convert_needed", false);
+        getServer().getPluginManager().registerEvents(this, this);
+        if (getServer().getMinecraftVersion() == "1.21.6" || getServer().getMinecraftVersion() == "1.21.7") {
+            getLogger().warning("1.21.6/1.21.7 Is only supported by ItemsAdder BETA.");
+        }
+        //TODO: IMPLEMENT COMMAND: /iageyser, reload: type: item, block, both
+        /*;this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            LiteralCommandNode<CommandSourceStack> buildCommand = Commands.literal("iageyser")
+                    .then(Commands.literal("reload")
+                        .executes(ctx ->{
+                            //TODO: IMPLEMENT PERMISSION CHECK
+                            reloadPlugin();
+                            return 0;
+                        })
+                    .then(Commands.argument("type", StringArgumentType.string()))
+                    .build();
+        });
+         */
+
+    }
+
+    @Override
+    public @NotNull Path getDataPath() {
+        return super.getDataPath();
+    }
+
+    @Override
     public void onDisable() {
-        getLogger().info("Iageyser disabled!");
+        getLogger().info("IaGeyser disabled!");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+
+        if (player.hasPermission("iageyser.seealerts")) {
+            player.sendMessage("§c[IAGeyser] §aThank you for using IAGeyser!");
+        }
     }
 
     @Subscribe
     public void onGeyserPostInitializeEvent(GeyserPostInitializeEvent event) {
         getLogger().info("GeyserMC Has Initialized!");
+        GeyserLoaded = true;
     }
     @Subscribe
     public void onGeyserDefineCustomItemsEvent(GeyserDefineCustomItemsEvent event) {
-        CustomItemOptions itemOptions = CustomItemOptions.builder()
-                .customModelData(1)
-                .name("my_item")
-                .damagePredicate(1) //This is a fractional value of damage/max damage and not a number between 0 and 1.
-                .unbreakable(true)
-                .build();
-        event.register(itemOptions);
+
+    }
+    @Subscribe
+    public void ResourcePackSendEvent(ResourcePackSendEvent event){
+        String URL;
+        URL = event.getUrl();
     }
 }
